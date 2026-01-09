d50cf30b9531b6d19142d9f483ce56d4643e0838
peilerx <peiler.dany@gmail.com>
2026-01-09 12:02:28 +0200

    bench: remove deltas, ALU substractions

--- benches/bench.rs
+++ benches/bench.rs
@@ -11,55 +11,41 @@ struct Point { x: f64, y: f64, z: f64 }
 struct Line(Point, Point);
 
 struct Lines {
-    ox: Vec<f64>, oy: Vec<f64>, oz: Vec<f64>,
-    dx: Vec<f64>, dy: Vec<f64>, dz: Vec<f64>,
+    tx: Vec<f64>, ty: Vec<f64>, tz: Vec<f64>,
 }
 
 #[inline(always)]
-pub fn simd_subs(origin: &[f64], direction: &[f64], t: f64, out: &mut [f64]) {
-    assert_eq!(origin.len(), direction.len());
-    assert_eq!(origin.len(), out.len());
+pub fn simd_subs(target: &[f64], t: f64, out: &mut [f64]) {
+    assert_eq!(target.len(), out.len());
 
     let t_v = f64x4::splat(t);
-
-    let (o_prefix, o_simd, o_suffix) = origin.as_simd::<4>();
-    let (d_prefix, d_simd, d_suffix) = direction.as_simd::<4>();
+    let (t_prefix, t_simd, t_suffix) = target.as_simd::<4>();
     let (out_prefix, out_simd_mut, out_suffix) = out.as_simd_mut::<4>();
 
-    o_prefix.iter()
-    .zip(d_prefix.iter())
-    .zip(out_prefix.iter_mut())
-    .for_each(|((&o, &d), res)| {
-        *res = f64::mul_add(d, t, o);
+    out_prefix.iter_mut().zip(t_prefix.iter()).for_each(|(res, &trg)| {
+        *res = *res + (trg - *res) * t;
     });
 
-    let mut o_chunks = o_simd.chunks_exact(8);
-    let mut d_chunks = d_simd.chunks_exact(8);
+    let mut t_chunks = t_simd.chunks_exact(8);
     let mut out_chunks = out_simd_mut.chunks_exact_mut(8);
 
-    for ((o_c, d_c), out_c) in o_chunks.by_ref().zip(d_chunks.by_ref()).zip(out_chunks.by_ref()) {
-        out_c[0] = d_c[0].mul_add(t_v, o_c[0]);
-        out_c[1] = d_c[1].mul_add(t_v, o_c[1]);
-        out_c[2] = d_c[2].mul_add(t_v, o_c[2]);
-        out_c[3] = d_c[3].mul_add(t_v, o_c[3]);
-        out_c[4] = d_c[4].mul_add(t_v, o_c[4]);
-        out_c[5] = d_c[5].mul_add(t_v, o_c[5]);
-        out_c[6] = d_c[6].mul_add(t_v, o_c[6]);
-        out_c[7] = d_c[7].mul_add(t_v, o_c[7]);
+    for (t_c, out_c) in t_chunks.by_ref().zip(out_chunks.by_ref()) {
+        out_c[0] = (t_c[0] - out_c[0]).mul_add(t_v, out_c[0]);
+        out_c[1] = (t_c[1] - out_c[1]).mul_add(t_v, out_c[1]);
+        out_c[2] = (t_c[2] - out_c[2]).mul_add(t_v, out_c[2]);
+        out_c[3] = (t_c[3] - out_c[3]).mul_add(t_v, out_c[3]);
+        out_c[4] = (t_c[4] - out_c[4]).mul_add(t_v, out_c[4]);
+        out_c[5] = (t_c[5] - out_c[5]).mul_add(t_v, out_c[5]);
+        out_c[6] = (t_c[6] - out_c[6]).mul_add(t_v, out_c[6]);
+        out_c[7] = (t_c[7] - out_c[7]).mul_add(t_v, out_c[7]);
     }
 
-    o_chunks.remainder().iter()
-    .zip(d_chunks.remainder().iter())
-    .zip(out_chunks.into_remainder().iter_mut())
-    .for_each(|((&ov, &dv), outv)| {
-        *outv = dv.mul_add(t_v, ov);
+    out_chunks.into_remainder().iter_mut().zip(t_chunks.remainder().iter()).for_each(|(outv, &tv)| {
+        *outv = (tv - *outv).mul_add(t_v, *outv);
     });
 
-    o_suffix.iter()
-    .zip(d_suffix.iter())
-    .zip(out_suffix.iter_mut())
-    .for_each(|((&o, &d), res)| {
-        *res = f64::mul_add(d, t, o);
+    out_suffix.iter_mut().zip(t_suffix.iter()).for_each(|(res, &trg)| {
+        *res = *res + (trg - *res) * t;
     });
 }
 
@@ -73,9 +59,7 @@ fn serial_subs_all_axis(lines: &[Line], t: f64, out: &mut [Point]) {
 }
 
 fn parallel_subs_all_axis(lines: &[Line], t: f64, out: &mut [Point]) {
-    lines.par_iter()
-    .zip(out.par_iter_mut())
-    .for_each(|(line, p)| {
+    lines.par_iter().zip(out.par_iter_mut()).for_each(|(line, p)| {
         p.x = line.0.x + (line.1.x - line.0.x) * t;
         p.y = line.0.y + (line.1.y - line.0.y) * t;
         p.z = line.0.z + (line.1.z - line.0.z) * t;
@@ -91,51 +75,66 @@ fn serial_subs_xy(lines: &[Line], t: f64, rx: &mut [f64], ry: &mut [f64]) {
 }
 
 fn parallel_subs_xy(lines: &[Line], t: f64, rx: &mut [f64], ry: &mut [f64]) {
-    lines.par_iter()
-    .zip(rx.par_iter_mut())
-    .zip(ry.par_iter_mut())
-    .for_each(|((line, out_x), out_y)| {
-        *out_x = line.0.x + (line.1.x - line.0.x) * t;
-        *out_y = line.0.y + (line.1.y - line.0.y) * t;
+    lines.par_iter().zip(rx.par_iter_mut()).zip(ry.par_iter_mut()).for_each(|((line, ox), oy)| {
+        *ox = line.0.x + (line.1.x - line.0.x) * t;
+        *oy = line.0.y + (line.1.y - line.0.y) * t;
     });
 }
+
 fn serial_prep_subs_all_axis(lines: &Lines, t: f64, rx: &mut [f64], ry: &mut [f64], rz: &mut [f64]) {
-    simd_subs(&lines.ox, &lines.dx, t, rx);
-    simd_subs(&lines.oy, &lines.dy, t, ry);
-    simd_subs(&lines.oz, &lines.dz, t, rz);
+    simd_subs(&lines.tx, t, rx);
+    simd_subs(&lines.ty, t, ry);
+    simd_subs(&lines.tz, t, rz);
 }
 
 fn parallel_prep_subs_all_axis(lines: &Lines, t: f64, rx: &mut [f64], ry: &mut [f64], rz: &mut [f64]) {
     rayon::scope(|s| {
-        s.spawn(|_| simd_subs(&lines.ox, &lines.dx, t, rx));
-        s.spawn(|_| simd_subs(&lines.oy, &lines.dy, t, ry));
-        simd_subs(&lines.oz, &lines.dz, t, rz);
+        s.spawn(|_| simd_subs(&lines.tx, t, rx));
+        s.spawn(|_| simd_subs(&lines.ty, t, ry));
+        simd_subs(&lines.tz, t, rz);
     });
 }
 
-fn serial_prep_subs_xy(ox: &[f64], dx: &[f64], oy: &[f64], dy: &[f64], t: f64, rx: &mut [f64], ry: &mut [f64]) {
-    simd_subs(ox, dx, t, rx);
-    simd_subs(oy, dy, t, ry);
+fn serial_prep_subs_xy(tx: &[f64], ty: &[f64], t: f64, rx: &mut [f64], ry: &mut [f64]) {
+    simd_subs(tx, t, rx);
+    simd_subs(ty, t, ry);
 }
 
-fn parallel_prep_subs_xy(ox: &[f64], dx: &[f64], oy: &[f64], dy: &[f64], t: f64, rx: &mut [f64], ry: &mut [f64]) {
-    rayon::join(
-        || simd_subs(ox, dx, t, rx),
-                || simd_subs(oy, dy, t, ry)
-    );
+fn parallel_prep_subs_xy(tx: &[f64], ty: &[f64], t: f64, rx: &mut [f64], ry: &mut [f64]) {
+    rayon::join(|| simd_subs(tx, t, rx), || simd_subs(ty, t, ry));
 }
 
+const BATCH_SIZE: usize = 30_000;
+
+fn serial_stream_subs_all_axis(lines: &Lines, t: f64, rx: &mut [f64], ry: &mut [f64], rz: &mut [f64]) {
+    for i in (0..lines.tx.len()).step_by(BATCH_SIZE) {
+        let end = (i + BATCH_SIZE).min(lines.tx.len());
+        simd_subs(&lines.tx[i..end], t, &mut rx[i..end]);
+        simd_subs(&lines.ty[i..end], t, &mut ry[i..end]);
+        simd_subs(&lines.tz[i..end], t, &mut rz[i..end]);
+    }
+}
+
+fn serial_stream_subs_xy(lines: &Lines, t: f64, rx: &mut [f64], ry: &mut [f64]) {
+    for i in (0..lines.tx.len()).step_by(BATCH_SIZE) {
+        let end = (i + BATCH_SIZE).min(lines.tx.len());
+        simd_subs(&lines.tx[i..end], t, &mut rx[i..end]);
+        simd_subs(&lines.ty[i..end], t, &mut ry[i..end]);
+    }
+}
 
 fn subs_bench(c: &mut Criterion) {
-    // let size = 30_000;
     let size = 20_000_000;
     let t = 0.42;
 
     let mut aos_lines = Vec::with_capacity(size);
     let mut soa_lines = Lines {
-        ox: Vec::with_capacity(size), oy: Vec::with_capacity(size), oz: Vec::with_capacity(size),
-        dx: Vec::with_capacity(size), dy: Vec::with_capacity(size), dz: Vec::with_capacity(size),
+        tx: Vec::with_capacity(size), ty: Vec::with_capacity(size), tz: Vec::with_capacity(size),
     };
+
+    let mut rx_init = vec![0.0; size];
+    let mut ry_init = vec![0.0; size];
+    let mut rz_init = vec![0.0; size];
 
     use std::f64::consts::PI;
     let golden_ratio = (1.0 + 5.0_f64.sqrt()) / 2.0;
@@ -147,17 +146,18 @@ fn subs_bench(c: &mut Criterion) {
         let z = phi.cos();
         let p0 = Point { x, y, z };
         let p1 = Point { x: x * 1.5, y: y * 1.5, z: z * 1.5 };
+
         aos_lines.push(Line(p0, p1));
-        soa_lines.ox.push(p0.x); soa_lines.oy.push(p0.y); soa_lines.oz.push(p0.z);
-        soa_lines.dx.push(p1.x - p0.x); soa_lines.dy.push(p1.y - p0.y); soa_lines.dz.push(p1.z - p0.z);
+        soa_lines.tx.push(p1.x); soa_lines.ty.push(p1.y); soa_lines.tz.push(p1.z);
+        rx_init[i] = p0.x; ry_init[i] = p0.y; rz_init[i] = p0.z;
     }
 
     let mut aos_out = vec![Point { x: 0.0, y: 0.0, z: 0.0 }; size];
-    let mut rx = vec![0.0; size];
-    let mut ry = vec![0.0; size];
-    let mut rz = vec![0.0; size];
+    let mut rx = rx_init.clone();
+    let mut ry = ry_init.clone();
+    let mut rz = rz_init.clone();
 
-    let mut group = c.benchmark_group("subs_bench_20M");
+    let mut group = c.benchmark_group("subs_bench_20M_DOD_Final");
     group.sample_size(10);
 
     // --- SECTION 1: SERIAL ---
@@ -171,7 +171,7 @@ fn subs_bench(c: &mut Criterion) {
     serial_prep_subs_all_axis(black_box(&soa_lines), black_box(t), black_box(&mut rx), black_box(&mut ry), black_box(&mut rz))
     ));
     group.bench_function("Serial_SoA_XY", |b| b.iter(||
-    serial_prep_subs_xy(black_box(&soa_lines.ox), black_box(&soa_lines.dx), black_box(&soa_lines.oy), black_box(&soa_lines.dy), black_box(t), black_box(&mut rx), black_box(&mut ry))
+    serial_prep_subs_xy(black_box(&soa_lines.tx), black_box(&soa_lines.ty), black_box(t), black_box(&mut rx), black_box(&mut ry))
     ));
 
     // --- SECTION 2: PARALLEL (2 CORES) ---
@@ -187,7 +187,15 @@ fn subs_bench(c: &mut Criterion) {
     parallel_prep_subs_all_axis(black_box(&soa_lines), black_box(t), black_box(&mut rx), black_box(&mut ry), black_box(&mut rz))
     ));
     group.bench_function("Parallel_SoA_XY", |b| b.iter(||
-    parallel_prep_subs_xy(black_box(&soa_lines.ox), black_box(&soa_lines.dx), black_box(&soa_lines.oy), black_box(&soa_lines.dy), black_box(t), black_box(&mut rx), black_box(&mut ry))
+    parallel_prep_subs_xy(black_box(&soa_lines.tx), black_box(&soa_lines.ty), black_box(t), black_box(&mut rx), black_box(&mut ry))
+    ));
+
+    // --- SECTION 3: STREAMING ---
+    group.bench_function("Stream_SoA_XYZ", |b| b.iter(||
+    serial_stream_subs_all_axis(black_box(&soa_lines), black_box(t), black_box(&mut rx), black_box(&mut ry), black_box(&mut rz))
+    ));
+    group.bench_function("Stream_SoA_XY", |b| b.iter(||
+    serial_stream_subs_xy(black_box(&soa_lines), black_box(t), black_box(&mut rx), black_box(&mut ry))
     ));
 
     group.finish();
